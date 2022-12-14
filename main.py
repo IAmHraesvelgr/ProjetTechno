ffb0c8f1bba5d21ffa5d176092f76e2bfffa64d1
IIIAKZIII <alexis.kahne@free.fr>
2022-12-14 12:28:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def main():
 
     message = input("Bienvenue dans ce programme de chiffrement de messages.\n\nVeuillez entrez le message à chiffrer: ")
 
-    if message == "":
-        message = input("Bienvenue dans ce programme de chiffrement de messages.\n\nVeuillez entrez le message à chiffrer: ")
+    while message == "":
+        message = input("\nVeuillez entrez le message à chiffrer: ")
 
 if __name__ == '__main__':
     main()
