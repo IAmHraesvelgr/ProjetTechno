2cbcc3ff56ffeacda4b0917c537a4bc24d57e749
IIIAKZIII <alexis.kahne@free.fr>
2022-12-14 15:58:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+# imports
 from termcolor import colored
 import os
 
+# Fonction Main
 def main():
     
     os.system("mode con cols=85 lines=30")
@@ -26,5 +28,6 @@ def main():
         print(colored("ERREUR : Veuillez renseigner un message à chiffrer.", 'red'))
         message = input("\nVeuillez entrez le message à chiffrer: \n")
 
+# Lancement du Programme
 if __name__ == '__main__':
     main()
